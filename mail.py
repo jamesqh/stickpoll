b916938a9f4465af5909af3b5e078604ae6e5afd
James <james.q.hogan@gmail.com>
2019-08-03 22:24:37 +0100

    Formatting

--- mail.py
+++ mail.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python3
+"""Module for sending emails."""
 
 from flask import current_app, g
 from flask_sendgrid import SendGrid
 
 def get_mail_context():
+    """Fetch or generate SendGrid mailer object."""
     if "main" not in g:
         mail = SendGrid(current_app)
         g.mail = mail
     return g.mail
 
 def send_mail(subject, html, sender, recipient):
+    """Send email."""
     mail = get_mail_context()
     r = mail.send_email(from_email=sender,
                         to_email=recipient,
